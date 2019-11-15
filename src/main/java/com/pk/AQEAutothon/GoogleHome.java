4ad21701811a5954c955416387d611b0f2907518
Pravin Kumar <KUPRAVIN@amdocs.com>
2019-11-15 18:10:02 +0530

    Updated Xpath

--- src/main/java/com/pk/AQEAutothon/GoogleHome.java
+++ src/main/java/com/pk/AQEAutothon/GoogleHome.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class GoogleHome {
 
 	public static By searchGoogle= By.name("q");
-	public static By searcBtn = By.xpath("//div[@class='FPdoLc VlcLAe']//input[@value='Google Search']");
+	public static By searcBtn = By.xpath("(//input[@value='Google Search'])[2]");
 	public static By viartWikiLink=By.xpath("//div[@class='g']//a[@href='https://en.wikipedia.org/wiki/Virat_Kohli']");
 	
 	
