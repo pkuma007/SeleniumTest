44e6d1cdf5ae84fba7753ca830ce7532262f6692
Pravin Kumar <KUPRAVIN@amdocs.com>
2019-11-15 16:48:23 +0530

    Added

--- src/main/java/com/pk/AQEAutothon/VKWikiPage.java
+++ src/main/java/com/pk/AQEAutothon/VKWikiPage.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.pk.AQEAutothon;
 
 import org.openqa.selenium.By;
@@ -14,20 +13,3 @@ public class VKWikiPage {
 	//th[text()='Career statistics']/../following::table[1]/tbody/tr
 	
 }
-=======
-package com.pk.AQEAutothon;
-
-import org.openqa.selenium.By;
-
-public class VKWikiPage {
-
-	public static By Competion = By.xpath("//th[text()='Career statistics']/../following::table/tbody//th[text()='Competition']/..");
-	public static By Matches = By.xpath("//th[text()='Career statistics']/../following::table/tbody//th[text()='Matches']/..");
-	public static By RunScored = By.xpath("//th[text()='Career statistics']/../following::table/tbody//th[text()='Runs scored']/..");
-	public static By BattingAvg = By.xpath("//th[text()='Career statistics']/../following::table/tbody//*[contains(text(),'Batting average')]/../..");
-	public static By VkImg = By.xpath("//img[contains(@src,'Virat_Kohli_portrait.jpg')]");
-	
-	//th[text()='Career statistics']/../following::table[1]/tbody/tr
-	
-}
->>>>>>> 2cff7cd9a4f01b5a9748fa6f542471223da31e67
