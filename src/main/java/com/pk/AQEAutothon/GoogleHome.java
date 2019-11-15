2cff7cd9a4f01b5a9748fa6f542471223da31e67
Pravin Kumar <KUPRAVIN@amdocs.com>
2019-11-15 14:46:37 +0530

    Added

---
+++ src/main/java/com/pk/AQEAutothon/GoogleHome.java
@@ -0,0 +1,12 @@
+package com.pk.AQEAutothon;
+
+import org.openqa.selenium.By;
+
+public class GoogleHome {
+
+	public static By searchGoogle= By.name("q");
+	public static By searcBtn = By.xpath("//div[@class='FPdoLc VlcLAe']//input[@value='Google Search']");
+	public static By viartWikiLink=By.xpath("//div[@class='g']//a[@href='https://en.wikipedia.org/wiki/Virat_Kohli']");
+	
+	
+}
---
+++ src/main/java/com/pk/AQEAutothon/VKWikiPage.java
@@ -0,0 +1,15 @@
+package com.pk.AQEAutothon;
+
+import org.openqa.selenium.By;
+
+public class VKWikiPage {
+
+	public static By Competion = By.xpath("//th[text()='Career statistics']/../following::table/tbody//th[text()='Competition']/..");
+	public static By Matches = By.xpath("//th[text()='Career statistics']/../following::table/tbody//th[text()='Matches']/..");
+	public static By RunScored = By.xpath("//th[text()='Career statistics']/../following::table/tbody//th[text()='Runs scored']/..");
+	public static By BattingAvg = By.xpath("//th[text()='Career statistics']/../following::table/tbody//*[contains(text(),'Batting average')]/../..");
+	public static By VkImg = By.xpath("//img[contains(@src,'Virat_Kohli_portrait.jpg')]");
+	
+	//th[text()='Career statistics']/../following::table[1]/tbody/tr
+	
+}
